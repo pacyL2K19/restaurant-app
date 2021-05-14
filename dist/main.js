501dfb6377236b125745e66baa1d75c06a30548d
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-14 18:38:24 +0200

    Minor update

--- dist/main.js
+++ dist/main.js
@@ -15,7 +15,7 @@
   \**********************/
 /***/ (() => {
 
-eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/preset-env'\\nRequire stack:\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/babel-loader/lib/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/loader-runner/lib/loadLoader.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/loader-runner/lib/LoaderRunner.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/NormalModule.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/NormalModuleFactory.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/Compiler.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/webpack.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack-cli/lib/webpack-cli.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack-cli/lib/bootstrap.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack-cli/bin/cli.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/bin/webpack.js\\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:925:15)\\n    at resolve (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/v8-compile-cache/v8-compile-cache.js:164:23)\\n    at resolveStandardizedName (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js:111:7)\\n    at resolvePreset (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js:59:10)\\n    at loadPreset (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js:78:20)\\n    at loadPreset.next (<anonymous>)\\n    at createDescriptor (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/config-descriptors.js:187:16)\\n    at createDescriptor.next (<anonymous>)\\n    at step (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/gensync/index.js:261:32)\\n    at evaluateAsync (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/gensync/index.js:291:5)\");\n\n//# sourceURL=webpack://restaurant-app/./src/index.js?");
+eval("console.log(\"Here we go ...\");\n\n//# sourceURL=webpack://restaurant-app/./src/index.js?");
 
 /***/ })
 
@@ -24,7 +24,7 @@ eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/li
 /******/ 	
 /******/ 	// startup
 /******/ 	// Load entry module and return exports
-/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
 /******/ 	var __webpack_exports__ = {};
 /******/ 	__webpack_modules__["./src/index.js"]();
 /******/ 	
