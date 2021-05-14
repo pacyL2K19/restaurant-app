872ca6f5fe12f71a1de71c0939b2fc3a355301b5
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-14 22:14:51 +0200

    Minor update

--- src/menu.js
+++ src/menu.js
@@ -1,17 +1,17 @@
+/* eslint-disable import/prefer-default-export */
 const renderMenuPage = (() => {
-  //container
-  const contentContainer = document.querySelector("#content");
-  const menu = document.createElement("div");
-  menu.setAttribute("id", "menu");
-  menu.setAttribute("data-tab-content", "");
+  const contentContainer = document.querySelector('#content');
+  const menu = document.createElement('div');
+  menu.setAttribute('id', 'menu');
+  menu.setAttribute('data-tab-content', '');
   menu.innerHTML = `
   <div class="hero">
   <h1>Menu</h1>
-  <img src="./images/chef.png" alt="" width="50" height="50" />
+  <img src="./assets/chef.png" alt="" width="50" height="50" />
 </div>
 <div class="menu-container">
   <div class="menu-item">
-    <img src="./images/hamburger.png" alt="" />
+    <img src="./assets/hamburger.png" alt="" />
     <div class="item-content">
       <h4>Hamburger: <span>$2.49</span></h4>
       <p>
@@ -21,35 +21,35 @@ const renderMenuPage = (() => {
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/cheeseburger.png" alt="" />
+    <img src="./assets/cheeseburger.png" alt="" />
     <div class="item-content">
       <h4>Cheeseburger: <span>$2.99</span></h4>
       <p>Similar to our hamburger, but with cheese.</p>
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/double-cheeseburger.png" alt="" />
+    <img src="./assets/double-cheeseburger.png" alt="" />
     <div class="item-content">
       <h4>Double Cheeseburger: <span>$3.49</span></h4>
       <p>Similar to our cheeseburger, but with an extra patty.</p>
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/steak.png" alt="" />
+    <img src="./assets/steak.png" alt="" />
     <div class="item-content">
       <h4>Steak: <span>$11.99</span></h4>
       <p>A juicy steak made just how you like it.</p>
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/ribs.png" alt="" />
+    <img src="./assets/ribs.png" alt="" />
     <div class="item-content">
       <h4>BBQ Ribs: <span>$8.99</span></h4>
       <p>Barbecue ribs with your choice of a add-ons.</p>
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/grilled-cheese.png" alt="" />
+    <img src="./assets/grilled-cheese.png" alt="" />
     <div class="item-content">
       <h4>Grilled Cheese Sandwich: <span>$4.99</span></h4>
       <p>
@@ -59,7 +59,7 @@ const renderMenuPage = (() => {
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/caesar-salad.png" alt="" />
+    <img src="./assets/caesar-salad.png" alt="" />
     <div class="item-content">
       <h4>Caesar Salad: <span>$7.99</span></h4>
       <p>
@@ -69,7 +69,7 @@ const renderMenuPage = (() => {
     </div>
   </div>
   <div class="menu-item">
-    <img src="./images/french-fries.png" alt="" />
+    <img src="./assets/french-fries.png" alt="" />
     <div class="item-content">
       <h4>French Fries: <span>$1.99</span></h4>
       <p>
