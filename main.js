f3f71fb37750cc1104156f0c740a6a13db50016e
Pacifique LINJANJA <pacilinja1@icloud.com>
2021-05-14 18:35:24 +0200

    Install dependencies

---
+++ main.js
@@ -0,0 +1,32 @@
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./src/index.js":
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/***/ (() => {
+
+eval("throw new Error(\"Module build failed (from ./node_modules/babel-loader/lib/index.js):\\nError: Cannot find module '@babel/preset-env'\\nRequire stack:\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/babel-loader/lib/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/loader-runner/lib/loadLoader.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/loader-runner/lib/LoaderRunner.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/NormalModule.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/NormalModuleFactory.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/Compiler.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/webpack.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/lib/index.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack-cli/lib/webpack-cli.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack-cli/lib/bootstrap.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack-cli/bin/cli.js\\n- /home/user/Documents/Microverse/Js/restaurant-app/node_modules/webpack/bin/webpack.js\\n    at Function.Module._resolveFilename (node:internal/modules/cjs/loader:925:15)\\n    at resolve (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/v8-compile-cache/v8-compile-cache.js:164:23)\\n    at resolveStandardizedName (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js:111:7)\\n    at resolvePreset (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js:59:10)\\n    at loadPreset (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/files/plugins.js:78:20)\\n    at loadPreset.next (<anonymous>)\\n    at createDescriptor (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/@babel/core/lib/config/config-descriptors.js:187:16)\\n    at createDescriptor.next (<anonymous>)\\n    at step (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/gensync/index.js:261:32)\\n    at evaluateAsync (/home/user/Documents/Microverse/Js/restaurant-app/node_modules/gensync/index.js:291:5)\");\n\n//# sourceURL=webpack://restaurant-app/./src/index.js?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module doesn't tell about it's top-level declarations so it can't be inlined
+/******/ 	var __webpack_exports__ = {};
+/******/ 	__webpack_modules__["./src/index.js"]();
+/******/ 	
+/******/ })()
+;
